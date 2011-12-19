c73e1cb72219ba17c75d6cdd23b2c13f4b7f1620
Alick Zhao <alick9188@gmail.com>
2011-12-19 15:52:22 +0800

    split js files; make html XHTML compatible

--- sample.js
+++ sample.js
@@ -60,45 +60,4 @@ var id = chrome.contextMenus.create({"title": title, "contexts":["selection"],
 //console.log("'" + context + "' item:" + id);
 var logging = 1;
 
-// from:
-// http://stackoverflow.com/questions/2153070/do-chrome-extensions-have-access-to-local-storage
-// Store item in local storage:
-function setItem(key, value) {
-    try {
-        log("Storing [" + key + ":" + value + "]");
-        window.localStorage.removeItem(key);      // <-- Local storage!
-        window.localStorage.setItem(key, value);  // <-- Local storage!
-    } catch(e) {
-        log("Error inside setItem");
-        log(e);
-    }
-    log("Return from setItem" + key + ":" +  value);
-}
 
-// Gets item from local storage with specified key.
-function getItem(key) {
-    var value;
-    log('Retrieving key [' + key + ']');
-    try {
-        value = window.localStorage.getItem(key);  // <-- Local storage!
-    }catch(e) {
-        log("Error inside getItem() for key:" + key);
-        log(e);
-        value = "null";
-    }
-    log("Returning value: " + value);
-    return value;
-}
-
-// Clears all key/value pairs in local storage.
-function clearStrg() {
-    log('about to clear local storage');
-    window.localStorage.clear(); // <-- Local storage!
-    log('cleared');
-}
-
-function log(txt) {
-    if(logging) {
-        console.log(txt);
-    }
-}
