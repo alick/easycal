20d4467967400ebef2e5497f93626346074577d2
Alick Zhao <alick9188@gmail.com>
2011-12-19 14:15:18 +0800

    sample.js: sketch of storing into localStorage
    Now we can store sth into the localStorage.
    TODO:
    * design the object to be stored.
      (note: value of localStorage must be string, so stringify needed)
    * what to use as the key of localStorage

--- sample.js
+++ sample.js
@@ -3,21 +3,81 @@
 
 // A generic onclick callback function.
 function genericOnClick(info, tab) {
-	var my_selection = info.selectionText;
-	var my_text = "";//content of calendar
-	var my_dates = "";//date YYMMDDThhmmssZ/YYMMDDThhmmssZ
-	var my_details = "";//detail of calendar
-	var my_location = "";//location of calendar
-	var my_trp = "false";
-	var my_siteurl = "wangheda.info";
-	var my_sitename = "HedaWang";
-	chrome.tabs.create({"url":"http://www.google.com/calendar/event?action=TEMPLATE&text="+my_selection});
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
+    var my_selection = info.selectionText;
+    var my_text = "";//content of calendar
+    var my_dates = "";//date YYMMDDThhmmssZ/YYMMDDThhmmssZ
+    var my_details = "";//detail of calendar
+    var my_location = "";//location of calendar
+    var my_trp = "false";
+    var my_siteurl = "wangheda.info";
+    var my_sitename = "HedaWang";
+
+    var this_time = new Date();
+    var timestamp = this_time.getTime();
+    var storekey = timestamp;
+
+    //chrome.tabs.create({"url":"http://www.google.com/calendar/event?action=TEMPLATE&text="+my_selection});
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+
+    // store into local storage
+    setItem(storekey, JSON.stringify(info));
+    var stored = getItem(storekey);
+    log("stored item: " + storekey + "=>" + stored);
+    var storedObj = JSON.parse(stored);
+    log("selectioned: " + storedObj.selectionText);
+
+    //clearStrg();
+    //stored = getItem('selection'); // should be null
+    //log("stored item:" + stored);
 }
 
 var title = chrome.i18n.getMessage("extMenuTitle");
 var id = chrome.contextMenus.create({"title": title, "contexts":["selection"],
                                        "onclick": genericOnClick});
 //console.log("'" + context + "' item:" + id);
+var logging = 1;
+
+// from:
+// http://stackoverflow.com/questions/2153070/do-chrome-extensions-have-access-to-local-storage
+// Store item in local storage:
+function setItem(key, value) {
+    try {
+        log("Storing [" + key + ":" + value + "]");
+        window.localStorage.removeItem(key);      // <-- Local storage!
+        window.localStorage.setItem(key, value);  // <-- Local storage!
+    } catch(e) {
+        log("Error inside setItem");
+        log(e);
+    }
+    log("Return from setItem" + key + ":" +  value);
+}
+
+// Gets item from local storage with specified key.
+function getItem(key) {
+    var value;
+    log('Retrieving key [' + key + ']');
+    try {
+        value = window.localStorage.getItem(key);  // <-- Local storage!
+    }catch(e) {
+        log("Error inside getItem() for key:" + key);
+        log(e);
+        value = "null";
+    }
+    log("Returning value: " + value);
+    return value;
+}
+
+// Clears all key/value pairs in local storage.
+function clearStrg() {
+    log('about to clear local storage');
+    window.localStorage.clear(); // <-- Local storage!
+    log('cleared');
+}
+
+function log(txt) {
+    if(logging) {
+        console.log(txt);
+    }
+}
