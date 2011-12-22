b89ce08648002f322121c9eef5834dd7b1f5cd72
Alick Zhao <alick9188@gmail.com>
2011-12-22 15:07:22 +0800

    popup: use jsDatePick to implement calendar
    * jsDatePick is a Javascript Canlendar utils licensed by GPL.
      So we can use it safely.
    * I haven't modified the jsDatePick lib, only changed the NEWLINE
      (CRLF to LF)

--- popup.js
+++ popup.js
@@ -1,5 +1,28 @@
-window.onload = addSchedule;
-var $ = function(id){return document.getElementById(id);}
+window.onload = function(){
+    g_globalObject = new JsDatePick({
+        useMode:1,
+        isStripped:true,
+        target:"calendar"
+        /*selectedDate:{
+          day:5,
+          month:9,
+          year:2006
+          },
+          yearsRange:[1978,2020],
+          limitToToday:false,
+          cellColorScheme:"beige",
+          dateFormat:"%m-%d-%Y",
+          imgPath:"img/",
+          weekStartDay:1*/
+    });
+
+    g_globalObject.setOnSelectedDelegate(function(){
+        var obj = g_globalObject.getSelectedDay();
+        console.log("a date was just selected and the date is : " + obj.day + "/" + obj.month + "/" + obj.year);
+        document.getElementById("calendar_result").innerHTML = obj.day + "/" + obj.month + "/" + obj.year;
+    });
+};
+
 function addSchedule () {
     var maxid_plus1 = getItem('sched_index');
     if (maxid_plus1 == null) {
@@ -17,6 +40,6 @@ function addSchedule () {
                          sched_time.toISOString() +
                          " " + schedule.summary +
                          "</div>";
-        $('#sched').innerHTML += sched_html;
+        document.getElementById('sched').innerHTML += sched_html;
     }
 }
