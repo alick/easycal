15765ff8586c22510ea26aab9db78c463b5ffbd9
Alick Zhao <alick9188@gmail.com>
2012-02-05 22:46:43 +0800

    editcal implementation, still have bug

--- data/editcal/editcal.js
+++ data/editcal/editcal.js
@@ -1,109 +1,84 @@
 console.debug('This is from editcal.js!');
+var sched_id = -1;
+var form_html = '';
+//$(document).ready(function(){
+    //form_html = $('#form_fill').html();
+//});
 
-/*
-    // Show pictures
-    $('#easycal-editcal #editcal_logo')[0].src = chrome.extension.getURL("huaci.png");//imgLogo;
-    $('#easycal-editcal #easycal-form-submit')[0].src = chrome.extension.getURL("easycal_img/save.png");//imgSave;
-    $('#easycal-editcal #easycal-form-cancel')[0].src = chrome.extension.getURL("easycal_img/cancel.png");//imgCancel;
-    */
+$('#easycal-form-cancel').on('click', function(){
+    self.port.emit('close');
+});
 
-/*
-    $('#easycal-editcal #easycal-form-cancel').bind('click', function(){
-        // Remove the form.
-        $('#easycal-editcal').remove();
-        $('#easycal-mist').remove();
-    });
+$('#easycal-form-submit').on('click', function(){
 
-    $('#easycal-editcal #easycal-form-submit').bind('click', function(){
+    var userYear = Number($('#easycal_year').val());
+    var userMonth = Number($('#easycal_month').val()-1);
+    var userDate = Number($('#easycal_day').val());
+    var userHour = Number($('#easycal_hour').val());
+    var userMinute = Number($('#easycal_minute').val());
+    var userSecond = 0; // Assume second is 0.
 
-        var userYear = Number($('#easycal_year').val());
-        var userMonth = Number($('#easycal_month').val()-1);
-        var userDate = Number($('#easycal_day').val());
-        var userHour = Number($('#easycal_hour').val());
-        var userMinute = Number($('#easycal_minute').val());
-        var userSecond = 0; // Assume second is 0.
+    var schedule = {};
+    if (sched_id === -1) {
+        console.warn('schedule id invalid');
+        return false;
+    }
+    schedule.id = sched_id;
+    var sched_time = new Date(userYear, userMonth, userDate, userHour, userMinute, userSecond);
+    // Checking time in the Javascript way.
+    if (sched_time.getFullYear() != userYear ||
+        sched_time.getMonth() != userMonth ||
+        sched_time.getDate() != userDate ||
+        sched_time.getHours() != userHour ||
+        sched_time.getMinutes() != userMinute ||
+        sched_time.getSeconds() != userSecond) {
 
-        g_schedule.sched_time = new Date(userYear, userMonth, userDate, userHour, userMinute, userSecond);
-        // Checking time in the Javascript way.
-        if (g_schedule.sched_time.getFullYear() != userYear ||
-                g_schedule.sched_time.getMonth() != userMonth ||
-                g_schedule.sched_time.getDate() != userDate ||
-                g_schedule.sched_time.getHours() != userHour ||
-                g_schedule.sched_time.getMinutes() != userMinute ||
-                g_schedule.sched_time.getSeconds() != userSecond) {
-
-            var origin_color = $("#easycal-editcal #div_time").css('background');
+            // Blink to give warning.
+            var origin_color = $("#div_time").css('background');
             for (var i=0; i<1200; i+= 400) {
-                setTimeout(function(){$("#easycal-editcal #div_time").css('background', 'red');}, i);
-                setTimeout(function(){$("#easycal-editcal #div_time").css('background', origin_color);}, i+200);
+                setTimeout(function(){$("#div_time").css('background', 'red');}, i);
+                setTimeout(function(){$("#div_time").css('background', origin_color);}, i+200);
             }
             return false;
         }
+    //schedule.sched_time = sched_time.getTime();
+    schedule.sched_time = sched_time;
 
-        g_schedule.loop = $('select[name=easycal_loop]').val();
+    schedule.loop = $('select[name=easycal_loop]').val();
 
-        g_schedule.content = $('#easycal_content').val();
-        g_schedule.type = $('input:radio[name=type]:checked').val();
-        //g_schedule.remind = $('select[name=remindUnit]').val();
+    schedule.content = $('#easycal_content').val();
+    schedule.type = $('input:radio[name=type]:checked').val();
+    //schedule.remind = $('select[name=remindUnit]').val();
 
-        g_schedule.timebefore = Number($('#easycal_remindTime').val());
-        var timebefore = Number($('#easycal_remindTime').val());
-        var timestyle=$('select[name=remindUnit]').val();
+    schedule.timebefore = Number($('#easycal_remindTime').val());
+    var timebefore = Number($('#easycal_remindTime').val());
+    var timestyle=$('select[name=remindUnit]').val();
 
-        g_schedule.timebefore = timebefore;
-        g_schedule.timestyle = timestyle;
+    schedule.timebefore = timebefore;
+    schedule.timestyle = timestyle;
 
-        //if(timestyle=="year") g_schedule.sched_remindtime = timebefore*1000*60*60*24*365;
-        //if(timestyle=="month") g_schedule.sched_remindtime = timebefore*1000*60*60*24*30;
-        
-        // g_schedule.sched_remindtime is the timestamp due to remind
-        g_schedule.sched_remindtime = new Date();
-        if(timestyle=="day") {
-            g_schedule.sched_remindtime.setTime(g_schedule.sched_time.getTime() - timebefore*1000*60*60*24);
-        }
-        if(timestyle=="hour") {
-            g_schedule.sched_remindtime.setTime(g_schedule.sched_time.getTime() - timebefore*1000*60*60);
-        }
-        if(timestyle=="minute") {
-            g_schedule.sched_remindtime.setTime(g_schedule.sched_time.getTime() - timebefore*1000*60);
-        }
-        //if(timestyle=="second") g_schedule.sched_remindtime = timebefore*1000;
+    // schedule.sched_remindtime is the timestamp due to remind
+    schedule.sched_remindtime = new Date();
+    if(timestyle=="day") {
+        schedule.sched_remindtime.setTime(schedule.sched_time.getTime() - timebefore*1000*60*60*24);
+    }
+    if(timestyle=="hour") {
+        schedule.sched_remindtime.setTime(schedule.sched_time.getTime() - timebefore*1000*60*60);
+    }
+    if(timestyle=="minute") {
+        schedule.sched_remindtime.setTime(schedule.sched_time.getTime() - timebefore*1000*60);
+    }
 
-        console.log('sched:');
-        console.log(g_schedule);
+    console.log('schedule from user input:');
+    console.log(JSON.stringify(schedule));
 
-        // store into local storage
-        console.log('Sending schedule...');
-        var request = {
-            newsched: true,
-            schedule_str: JSON.stringify(g_schedule),
-        };
-        chrome.extension.sendRequest(request, function(response) {
-            console.log(response.farewell);
-            if (response.farewell === "OK. I got it.") {
-                console.log("Your schedule has been successfully saved ^_^");
-                
-                var pic_height = $('#form_fill').css('height');
-                var pic_width = $('#form_fill').css('width');
-                $('#form_fill').html("<img alt='saving' src='"+chrome.extension.getURL("easycal_img/saving_ok.png")+"' height='"+pic_height+"' style='padding:0;margin:0;border:0;'>");
-                $('#form_fill').css("height", pic_height);
-                $('#form_fill').css("width", pic_width);
-                $('#form_fill').css("text-align", "center");
-                $('#fieldset_easycal').css('text-align', 'center');
-                                        
-                setTimeout(
-                    function(){
-                        $('#easycal-editcal').remove();
-                        $('#easycal-mist').remove();
-                        document.body.style.overflowY=origin_overflowY;
-                    },
-                    1000);
-            }
-        });
+    self.port.emit('save', schedule);
 
-        return false;
-    });
+ 
+    return false;
+});
 
+/*
     $('#easycal-editcal #easycal-form-submit').bind('mouseenter', function(){
         $('#easycal-editcal #easycal-form-submit')[0].src = chrome.extension.getURL("easycal_img/save_mouseover.png");//imgSave_onmouseover;
     });
@@ -119,21 +94,42 @@ console.debug('This is from editcal.js!');
 });
 */
 
-self.on('message', function(schedule) {
-    console.log("begin to fill the form");
+self.port.on('fillform', function(schedule) {
+    console.debug("begin to fill the form...");
     console.log("schedule: " + JSON.stringify(schedule));
-    if (schedule) {
-        console.log('Filling the form...');
-        /*
-        var time = new Date(schedule.sched_time);
-        $('#easycal-editcal #easycal_year').val(time.getFullYear());
-        $('#easycal-editcal #easycal_month').val(time.getMonth() + 1);
-        $('#easycal-editcal #easycal_day').val(time.getDate());
-        $('#easycal-editcal #easycal_hour').val(time.getHours());
-        $('#easycal-editcal #easycal_minute').val(time.getMinutes());
+    sched_id = schedule.id;
+    if (form_html) {
+        $('#form_fill').html(form_html);
+    } else {
+        console.warn('form_html wrong');
+    }
+    var time = new Date(schedule.sched_time);
+    $('#easycal_year').val(time.getFullYear());
+    $('#easycal_month').val(time.getMonth() + 1);
+    $('#easycal_day').val(time.getDate());
+    $('#easycal_hour').val(time.getHours());
+    $('#easycal_minute').val(time.getMinutes());
 
-        $('#easycal-editcal #easycal_content').val(schedule.content);
-        $('#easycal-editcal #easycal_remindTime').val('15');
-        */
+    $('#easycal_content').val(schedule.content);
+    $('#easycal_remindTime').val('15');
+});
+
+self.port.on('save_response', function(response) {
+    if (response === "OK") {
+        console.log("Your schedule has been successfully saved ^_^");
+
+        var saved_html = "<img alt='saving' src='saving_ok.png' style='padding:0;margin:0;border:0;' />";
+        form_html = $('#form_fill').html();
+        $("#form_fill").html(saved_html);
+        console.debug('form_fill:' + $('#form_fill').html());
+
+        setTimeout(
+            function(){
+                self.port.emit('close');
+            },
+            1000
+            );
+    } else {
+        console.log("Your schedule has NOT been successfully saved -_-");
     }
 });
